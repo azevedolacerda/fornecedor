ec83eaa581f4997857053c7b54487166ff17b8c0
azevedo.lacerda <azevedo.lacerda@hotmail.com.br>
2020-08-21 16:05:41 -0300

    Inserido erro de teste unitário

--- src/test/java/br/com/home/microsservices/FornecedorApplicationTests.java
+++ src/test/java/br/com/home/microsservices/FornecedorApplicationTests.java
@@ -1,5 +1,7 @@
 package br.com.home.microsservices;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,7 @@ class FornecedorApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertEquals("Teste", "");
 	}
 
 }
